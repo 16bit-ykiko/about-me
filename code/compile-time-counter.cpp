2bf3ba7c789e4657349bbc9ed700fa9251a07a6e
ykiko <ykikoykikoykiko@gmail.com>
2024-05-24 19:54:25 +0800

    Update.

--- code/compile-time-counter.cpp
+++ code/compile-time-counter.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 
 template <std::size_t N>
-struct reader
-{
+struct reader {
     friend auto counted_flag(reader<N>);
 };
 
 template <std::size_t N>
-struct setter
-{
-    friend auto counted_flag(reader<N>) {}
-
+struct setter {
     std::size_t value = N;
+
+    friend auto counted_flag(reader<N>) {}
 };
 
-template <auto N = 0, auto tag = [] {}, bool condition = requires(reader<N> red) { counted_flag(red); }>
-consteval auto next()
-{
-    if constexpr(!condition)
-    {
+template <auto N = 0,
+          auto tag = [] {},
+          bool condition = requires(reader<N> red) { counted_flag(red); }>
+consteval auto next() {
+    if constexpr(!condition) {
         constexpr setter<N> s;
         return s.value;
+    } else {
+        return next<N + 1>();
     }
-    else { return next<N + 1>(); }
 }
 
-int main()
-{
+int main() {
     constexpr auto a = next();
     constexpr auto b = next();
     constexpr auto c = next();
