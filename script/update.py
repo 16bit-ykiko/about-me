7033ddbbf2f74c74e7b6be6ec977b9e1954e7a5f
ykiko <ykikoykikoykiko@gmail.com>
2024-05-25 14:41:58 +0800

    Test.

--- script/update.py
+++ script/update.py
@@ -15,10 +15,6 @@ def get(url: str):
     n = 0
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-        'Sec-Ch-Ua-Platform': 'Windows',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding': 'br',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
     }
     while n < 5:
         try:
